744a1b176c4ad84a61d0273eb793e079a0343c8e
Roberto Rodriguez <rrodri0622@gmail.com>
2023-07-19 00:39:16 -0400

    Updated CK Orchestrator script

--- functionapp/Orchestrator/run.ps1
+++ functionapp/Orchestrator/run.ps1
@@ -11,70 +11,7 @@ $functionMap = @{
     WindowsHybridWorker = 'WinHybridWorkerActivity'
 }
 # Define variables
-$SimuProps = $Simulation.psobject.properties
 $SimuSteps = $Simulation.steps
-
-# Define Functions
-function Set-SimuReferences ($Simulation,$SimuSteps,$ReferenceName) {
-    foreach ($Step in $SimuSteps){
-        Write-Host "  [>] Processing $($Step.Name) step.." 
-        if ($Step.execution.psobject.properties.Name -contains 'parameters') {
-            $StepParameters = $Step.execution.parameters
-            foreach ($key in $StepParameters.psobject.properties.Name){
-                $currentParamValue = $StepParameters.$key.defaultValue
-                if ($currentParamValue -like ('*{0}(*)*' -f $ReferenceName)) {
-                    $currentParamValue -match "$ReferenceName\((?<refName>[a-zA-Z]{1,})\)" | Out-Null
-                    $paramName = $matches['refName']
-                    if ($ReferenceName -eq 'parameters'){
-                        $paramValue = $simulation.$ReferenceName.$paramName.defaultValue
-                    } else {
-                        $paramValue = $simulation.$ReferenceName.$paramName
-                    }
-                    $newParamValue = $currentParamValue -replace ('({0}\({1}\))' -f $ReferenceName,$paramName) , $paramValue
-                    $StepParameters.$key.defaultValue = $newParamValue
-                }
-            }
-        }
-    }
-    $SimuSteps
-}
-
-# Processing Parameters
-if ($SimuProps.Name -contains 'parameters'){
-    # Validate if default values are set
-    Write-Host "[*] Checking if Parameters have a default value set.."
-    foreach ($param in $Simulation.parameters.psobject.properties.Name){
-        if (-not ($Simulation.parameters.$param.psobject.properties.Name -contains 'defaultValue')) {
-            Write-Error "[Parameter $param] does not have a value set."
-            return
-        }
-    }
-    # Processing Variables
-    if ($SimuProps.Name -contains 'variables'){
-        Write-Debug "[*] Resolving global parameters in global variables"
-        foreach ($key in $Simulation.variables.psobject.properties.Name) {
-            Write-Debug "  [>] Processing $key variable.."
-            $currentVarValue = $Simulation.variables.$key
-            if ($currentVarValue -like '*parameters(*)*') {
-                $currentVarValue -match "parameters\((?<refName>[a-zA-Z]{1,})\)" | Out-Null
-                $paramName = $matches['refName']
-                $paramValue = $Simulation.parameters.$paramName.defaultValue
-                $newParamValue = $currentVarValue -replace ('(parameters\({0}\))' -f $paramName) , $paramValue
-                $Simulation.variables.$key = $newParamValue
-            }
-        }
-    }
-    # Processing Simulation Steps
-    Write-Host "[*] Resolving global parameters in step parameters"
-    $SimuSteps = Set-SimuReferences $Simulation $SimuSteps 'parameters'
-}
-
-# Processing Variables
-if ($SimuProps.Name -contains 'variables'){
-    # Processing Steps
-    Write-Host "[*] Resolving global variables in step parameters"
-    $SimuSteps = Set-SimuReferences $Simulation $SimuSteps 'variables'
-}
 
 # Processing simulation steps
 write-host "[*] Processing simulation steps.."
